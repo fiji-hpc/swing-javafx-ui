3a66de80d7be1fed0b87c6421c210dc01fb4edc6
Dimitrios Stefanos Velissariou <velissarious@gmail.com>
2020-10-30 16:12:55 +0100

    allow empty text in numeric spinner while focused else set lower value

--- src/main/java/cz/it4i/swing_javafx_ui/SimpleControls.java
+++ src/main/java/cz/it4i/swing_javafx_ui/SimpleControls.java
@@ -10,22 +10,39 @@ public class SimpleControls {
 		// Private constructor to hide the implicit public one.
 	}
 
+	private static void setValue(Spinner<Integer> spinner, int lowerBound) {
+		final int cleanValue = lowerBound;
+		JavaFXRoutines.runOnFxThread(() -> {
+			spinner.getEditor().setText("" + cleanValue);
+			spinner.getValueFactory().setValue(cleanValue);
+		});
+	}
+
 	public static void spinnerIgnoreNoneNumericInput(Spinner<Integer> spinner,
 		int lowerBound, int upperBound)
 	{
+		spinner.getEditor().focusedProperty().addListener(event -> {
+			boolean isFocused = spinner.isFocused();
+			boolean isEmpty = spinner.getEditor().getText().isEmpty();
+			if (!isFocused && isEmpty) {
+				setValue(spinner, lowerBound);
+			}
+		});
+
 		spinner.getEditor().textProperty().addListener((
 			ObservableValue<? extends String> observable, String oldValue,
 			String newValue) -> {
+			if (newValue.isEmpty()) {
+				return;
+			}
 
-				// Value should be numeric only:
-				if (!newValue.matches("\\d*"))
-				{
+			// Value should be numeric only:
+			if (!newValue.matches("\\d*")) {
 				JavaFXRoutines.runOnFxThread(() -> spinner.getEditor().setText(newValue
 					.replaceAll("[^\\d]", "")));
 			}
-				// Value should be within the boundaries.
-				try
-				{
+			// Value should be within the boundaries.
+			try {
 				int temp = Integer.parseInt(newValue);
 				if (temp < lowerBound) {
 					temp = lowerBound;
@@ -33,17 +50,13 @@ public class SimpleControls {
 				else if (temp > upperBound) {
 					temp = upperBound;
 				}
-				final int cleanValue = temp;
-				JavaFXRoutines.runOnFxThread(() -> {
-					spinner.getEditor().setText("" + cleanValue);
-					spinner.getValueFactory().setValue(cleanValue);
-				});
+				setValue(spinner, temp);
 			}
 			catch (Exception e) {
 				// Value should be an integer.
 				JavaFXRoutines.runOnFxThread(() -> spinner.getEditor().setText("" +
 					oldValue));
 			}
-			});
+		});
 	}
 }
